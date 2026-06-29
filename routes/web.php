ecdb847aa8ebc6ed3758a7cccab13fa22bddc0f7
rajae59 <elmimounirajae59@gmail.com>
2026-06-29 23:56:57 +0200

    Fix web routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    return response()->json(['message' => 'API RestaurantPro'], 200);
-})->where('any', '.*');
+Route::get('/', function () {
+    return response()->json(['message' => 'API RestaurantPro']);
+});
